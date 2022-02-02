7fcf74555befc974d20319af93df4ecb282a55e5
Oriol <oriol.orus.or@gmail.com>
2022-02-02 23:03:06 +0100

    add card component

---
+++ FrontWeb/src/components/Card/card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./card.styles.css";
+
+const Card = () => {
+  return (
+  <div className="cardContainer">
+      <p>Player</p>
+      <p>0</p>
+      <button>+</button>
+      <button>-</button>
+  </div>
+  )   
+};
+
+export default Card;
