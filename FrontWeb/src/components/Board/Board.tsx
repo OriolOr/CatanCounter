e9c25998dda604b0564f2941c64ad440128abd9a
RBAMOUSER\orusordo <oriol.orus_ordonez@roche.com>
2022-02-03 00:50:33 +0100

    add counter buttons

--- FrontWeb/src/components/Board/Board.tsx
+++ FrontWeb/src/components/Board/Board.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./board.styles.css";
-import Card from "../Card/card"
+import Card from "../Card/card";
 
-const Board = () => {
-  return <div className="boardContainer">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    
-  </div>;
+const Board: React.FC = () => {
+  return (
+    <div className="boardContainer">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default Board;
--- FrontWeb/src/components/Card/card.tsx
+++ FrontWeb/src/components/Card/card.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./card.styles.css";
 
-const Card = () => {
+const Card: React.FC = () => {
+  const [player, setPlayer] = useState("Player");
+  const [points, setPoints] = useState(0);
+
   return (
-  <div className="cardContainer">
-      <p>Player</p>
-      <p>0</p>
-      <button>+</button>
-      <button>-</button>
-  </div>
-  )   
+    <div className="cardContainer">
+      <p className="player">{player}</p>
+      <p className ="points">{points}</p>
+      <button onClick={addPoint}>+</button>
+      <button onClick={subPoint}>-</button>
+    </div>
+  );
+
+  function addPoint() {
+    setPoints(points + 1);
+  }
+
+  function subPoint() {
+    if (points > 0) {
+      setPoints(points - 1);
+    }
+  }
 };
 
 export default Card;
