43dd654ccf079b74e2fad6ee3fda56f339bb973a
Oriol <oriol.orus.or@gmail.com>
2022-03-30 23:35:48 +0200

    add buttons

--- frontweb/src/components/board/board.tsx
+++ frontweb/src/components/board/board.tsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./board.styles.css";
 import Card from "../card/card";
 
 const Board: React.FC = () => {
+  
+  const [playerList, setPlayer] = useState([]);
   return (
-    <div className="boardContainer">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <React.Fragment>
+      <div className="boardContainer">
+        <Card />
+        <Card />
+        <Card />
+ 
+      </div>
+      <div className ="addButtonContainer">
+        <button onClick={AddPlayer}>Add</button>
+        <button onClick={RemovePlayer}>Remove</button>
+        
+      </div>
+    </React.Fragment>
+
+
   );
+
+  function AddPlayer(){
+    console.log("AddPlayer") 
+  }
+
+  function RemovePlayer(){
+    console.log("RemvovePlayerer") 
+  }
+
 };
 
 export default Board;
